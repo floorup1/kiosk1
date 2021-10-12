b2eb80826a3a0a248660e20eb548c9a4d54f7537
floorup1 <42138394+floorup1@users.noreply.github.com>
2021-10-12 11:34:16 +0400

    Create kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# sudo apt-get update -y
+# sudo apt-get upgrade -y
+sudo curl -o /opt/kiosk.sh https://raw.githubusercontent.com/floorup1/kiosk1/master/kiosk.sh
+# sudo rm -rf /home/kiosk-user/share/*
+sudo rm -rf startpage
+sudo curl -LOk https://github.com/floorup1/startpage/archive/master.zip
+unzip -o master.zip
+sudo cp -r startpage-master startpage
+xset -dpms
+xset s off
+openbox-session &
+start-pulseaudio-x11
+
+while true; do
+sudo curl -o /etc/chromium/policies/managed/policy.json https://raw.githubusercontent.com/floorup1/kiosk/master/policy.json
+rm -rf /home/kiosk-user/.config/chromium/*
+rm -rf /home/kiosk-user/.cache/chromium/*
+chromium --start-maximized 'file:///home/kiosk-user/startpage/start.html'
+done
